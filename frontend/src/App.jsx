6ab303013dcf8969a12073b39c3108573e28be84
harshita <harshita.maheshwari@glsuniversity.ac.in>
2025-11-19 10:19:52 +0530

    nov19

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import { Link, Outlet } from "react-router"
 function App() {
    return (
     <>
+    <Link to="/" >Home</Link> &emsp;
     <Link to="/add" >Add User</Link> &emsp;
     <Link to='/view'>View Users</Link>
     <div>
--- frontend/src/components/UserList.jsx
+++ frontend/src/components/UserList.jsx
@@ -24,8 +24,39 @@ const handleDelete = async(id)=>{
   }
   catch(err){console.log(err)}
 }
+//search
+const [search,setSearch] = useState("")
+const [sortType,setSortType] = useState("")
+
+const filterUsers = users.filter((user)=>user.name.toLowerCase().includes(search.toLowerCase()))
+.sort((a,b)=>{
+  if(sortType=="atoz") {return a.name.localeCompare(b.name)}
+  if(sortType=="ztoa") {return b.name.localeCompare(a.name)}
+  if(sortType=="ltoh") {return a.age - b.age}
+  if(sortType=="htol") {return b.age - a.age}
+  return 0;
+})
+
+// users.sort((a,b)=>a-b) //asending
+// users.sort((a,b)=>b-a) //desending
+
+
   return (
     <div>
+      {/* search */}
+      <input placeholder='search by name' 
+      value={search} onChange={(e)=>setSearch(e.target.value)} />
+      &emsp;
+      <select value={sortType} onChange={(e)=>setSortType(e.target.value)}>
+        <option value="">Choose</option>
+        <option value="atoz">Name (A-Z)</option>
+        <option value="ztoa">Name (Z-A)</option>
+        <option value="ltoh">Age (Ascending)</option>
+        <option value="htol">Age (Decending)</option>
+      </select>    
+      
+      <br/>
+
       <h3>User List</h3>
       <table border="1" cellSpacing={0}>
         <thead>
@@ -35,10 +66,23 @@ const handleDelete = async(id)=>{
             </tr>
         </thead>
         <tbody>
-            {users.length === 0 ? <tr><td colSpan={5}>No users found</td></tr>
+            {filterUsers.length === 0 ? <tr><td colSpan={5}>No users found</td></tr>
             :
             <>
-        {users.map((user) => 
+        {/* {users.map((user) => 
+            <tr key={user._id}>
+                <td>{user._id}</td>
+                <td>{user.name}</td>
+                <td>{user.email}</td>
+                <td>{user.age}</td>
+                <td><button 
+                onClick={()=>navigate(`/edit/${user._id}`)}>Edit</button>{" "}
+              <button 
+              onClick={()=>handleDelete(user._id)}>
+                Delete</button></td>
+            </tr>)} */}
+
+            {filterUsers.map((user) => 
             <tr key={user._id}>
                 <td>{user._id}</td>
                 <td>{user.name}</td>
